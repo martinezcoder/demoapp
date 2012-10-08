07924db30655033442457be1ef6421e6ea219d43
Fran LP <fran.martinez@socialwin.es>
2012-10-08 18:59:32 +0200

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'b4060b06349c7ecea7b9e07d3c76c020d2f929c29a206aa2966188bf89bf032293318e875a1361f413d4b3c3e8fdb3fc86c567f2474432d73de0988c2713775c'
