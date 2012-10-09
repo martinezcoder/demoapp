24fddf71b7dccebd747406af4734950464313aae
Fran LP <fran.martinez@socialwin.es>
2012-10-09 15:54:04 +0200

    Finish demo_app

---
+++ generate/config/initializers/cookie_verification_secret.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.cookie_verifier_secret = '325277bd266b5c2c0a74f6049dfb59b19719ad72bf1064b30a50c3a8df75ddf6363ee95c1dacfb8bb7428f02fd0df1721345bd46bde3a40fbb122e586e3d0f6f';
